7d6e019475e74373d68512f6a5a156df6c31055a
jong lee <lsljw94@daum.net>
2022-07-06 20:04:55 +0900

    pc-nav

--- js/common.js
+++ js/common.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-   
+   $(".mobile-bt").click(function(){
+    $(".mobile-gnb").show();
+   })
+   $(".mobile-close").click(function(){
+    $(".mobile-gnb").hide();
+   });
 });
